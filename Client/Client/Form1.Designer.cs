56d4186772371408290a438e03dda777ece0d898
Kerim Demir <kerim.demir2002@hotmail.com>
2023-12-01 15:35:59 +0300

    client design changes added

--- Client/Client/Form1.Designer.cs
+++ Client/Client/Form1.Designer.cs
@@ -35,11 +35,21 @@
             this.textBox_name = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.button_send = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
-            this.label1.Location = new System.Drawing.Point(118, 116);
+            this.label1.Location = new System.Drawing.Point(166, 41);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(75, 15);
             this.label1.TabIndex = 0;
@@ -48,7 +58,7 @@
             // textBox_port
             // 
             this.textBox_port.AccessibleName = "textBox_port";
-            this.textBox_port.Location = new System.Drawing.Point(262, 113);
+            this.textBox_port.Location = new System.Drawing.Point(310, 38);
             this.textBox_port.Name = "textBox_port";
             this.textBox_port.Size = new System.Drawing.Size(150, 22);
             this.textBox_port.TabIndex = 1;
@@ -56,7 +66,7 @@
             // button_connect
             // 
             this.button_connect.AccessibleName = "button_connect";
-            this.button_connect.Location = new System.Drawing.Point(494, 107);
+            this.button_connect.Location = new System.Drawing.Point(542, 35);
             this.button_connect.Name = "button_connect";
             this.button_connect.Size = new System.Drawing.Size(102, 28);
             this.button_connect.TabIndex = 2;
@@ -67,14 +77,14 @@
             // textBox_name
             // 
             this.textBox_name.AccessibleName = "textBox_name";
-            this.textBox_name.Location = new System.Drawing.Point(262, 168);
+            this.textBox_name.Location = new System.Drawing.Point(310, 93);
             this.textBox_name.Name = "textBox_name";
             this.textBox_name.Size = new System.Drawing.Size(150, 22);
             this.textBox_name.TabIndex = 3;
             // 
             // label2
             // 
-            this.label2.Location = new System.Drawing.Point(118, 171);
+            this.label2.Location = new System.Drawing.Point(166, 96);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(75, 15);
             this.label2.TabIndex = 4;
@@ -83,7 +93,7 @@
             // button_send
             // 
             this.button_send.AccessibleName = "button_send";
-            this.button_send.Location = new System.Drawing.Point(494, 171);
+            this.button_send.Location = new System.Drawing.Point(542, 90);
             this.button_send.Name = "button_send";
             this.button_send.Size = new System.Drawing.Size(102, 28);
             this.button_send.TabIndex = 5;
@@ -91,11 +101,109 @@
             this.button_send.UseVisualStyleBackColor = true;
             this.button_send.Click += new System.EventHandler(this.button_send_Click);
             // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(53, 243);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(140, 166);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(422, 243);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(140, 166);
+            this.richTextBox2.TabIndex = 7;
+            this.richTextBox2.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.AccessibleName = "button_connect";
+            this.button1.Location = new System.Drawing.Point(72, 415);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 28);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "connect";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.AccessibleName = "button_connect";
+            this.button2.Location = new System.Drawing.Point(441, 415);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(102, 28);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "connect";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(72, 225);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 15);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "IF100";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(441, 225);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 15);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "SPS101";
+            // 
+            // textBox1
+            // 
+            this.textBox1.AccessibleName = "textBox_port";
+            this.textBox1.Location = new System.Drawing.Point(199, 287);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(206, 22);
+            this.textBox1.TabIndex = 12;
+            // 
+            // textBox2
+            // 
+            this.textBox2.AccessibleName = "textBox_port";
+            this.textBox2.Location = new System.Drawing.Point(568, 287);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(206, 22);
+            this.textBox2.TabIndex = 13;
+            // 
+            // button3
+            // 
+            this.button3.AccessibleName = "button_connect";
+            this.button3.Location = new System.Drawing.Point(199, 315);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(102, 28);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "connect";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.AccessibleName = "button_connect";
+            this.button4.Location = new System.Drawing.Point(568, 315);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(102, 28);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "connect";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.button_send);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox_name);
@@ -107,6 +215,20 @@
             this.ResumeLayout(false);
             this.PerformLayout();
         }
+
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+
+        private System.Windows.Forms.TextBox textBox2;
+
+        private System.Windows.Forms.TextBox textBox1;
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox_port;
